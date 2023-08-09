a54ba796c7b23b60e66af0a375bfa2ce0e191bee
Luiz Carlos Carchedi <carchedi.ufjf@gmail.com>
2023-08-09 14:24:29 -0300

    final commit

--- automation.py
+++ automation.py
@@ -1,21 +1,24 @@
 import pyautogui
 import pyperclip
 
-pyautogui.hotkey("alt", "tab")
-pyautogui.hotkey("ctrl", "t")
-pyautogui.PAUSE = 2 
-pyperclip.copy("www.gmail.com")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.hotkey("enter")
-pyautogui.moveTo(220, 320)
-pyautogui.click()
-pyperclip.copy("carchedi.ufjf@gmail.com")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.hotkey("tab")
-pyperclip.copy("Email de teste")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.hotkey("tab")
-pyperclip.copy("Enviando o primeiro email automatizado com o python!!!")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.hotkey("tab")
-pyautogui.hotkey("enter")
+def sending_email(to, subject, message):
+    pyautogui.hotkey("alt", "tab")
+    pyautogui.hotkey("ctrl", "t")
+    pyautogui.PAUSE = 2 
+    pyperclip.copy("www.gmail.com")
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.hotkey("enter")
+    pyautogui.moveTo(150, 230)
+    pyautogui.click()
+    pyperclip.copy(to)
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.hotkey("tab")
+    pyperclip.copy(subject)
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.hotkey("tab")
+    pyperclip.copy(message)
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.hotkey("tab")
+    pyautogui.hotkey("enter")
+
+sending_email('lcarchedi@hotmail.com', 'email to hotmail', 'if this message arrives, it works!')
