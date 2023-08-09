5d855ccad81da3abb4fb91e3124688c049264f6a
Luiz Carlos Carchedi <carchedi.ufjf@gmail.com>
2023-08-09 11:33:39 -0300

    first automation task with python worksgit add . <3

---
+++ automation.py
@@ -0,0 +1,22 @@
+import pyautogui
+import pyperclip
+
+pyautogui.hotkey("alt", "tab")
+pyautogui.hotkey("ctrl", "t")
+pyautogui.PAUSE = 2 
+pyperclip.copy("www.gmail.com")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("enter")
+pyautogui.moveTo(220, 320)
+pyautogui.click()
+pyperclip.copy("carchedi.ufjf@gmail.com")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("tab")
+pyperclip.copy("Email de teste")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("tab")
+pyperclip.copy("Enviando o primeiro email automatizado com o python!!!")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("tab")
+pyautogui.hotkey("enter")
+
